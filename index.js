599569d88b83371ad66f7ddba21a3942c88a4073
sergio pinto <sepintoedf21@bootcamp.ucamp.io>
2023-10-23 13:13:07 -0700

    Practica getElementById()

---
+++ index.js
@@ -0,0 +1,11 @@
+// Seleccionamos el elemento a través de su ID
+const main = document.getElementById("main")
+
+// Verificamos en consola que obtenemos la etiqueta
+console.log(main)
+// Seleccionamos los elementos a través de su clase
+const main = document.getElementsByClassName("main")
+
+// Verificamos en consola que obtenemos la etiqueta
+console.log(main[0])
+console.log(main[0].textContent)
